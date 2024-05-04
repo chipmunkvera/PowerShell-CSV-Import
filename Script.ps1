5856a8816aa7e92c19f55df327942fcd96d2a27e
chipmunkvera <56935110+chipmunkvera@users.noreply.github.com>
2024-05-03 20:31:50 -0400

    Create Script.ps1

---
+++ Script.ps1
@@ -0,0 +1,42 @@
+# Obtain the csv file
+$file = Read-Host "Enter CSV path"
+
+$file = Import-Csv $file
+
+#obtain values from file
+foreach ($User in $file)
+{
+    $fname = $User.Firstname
+    $lname = $User.Lastname
+    $dept = $User.department
+    $role = $User.role
+    $username = ($fname + $lname).ToLower()
+    $email = $username + '@' + 'org.net'
+    $temppass = (convertto-securestring ("Pa$$w0rd!") -AsPlainText -Force) 
+
+    #check if user exists
+    if (Get-ADUser -F {name -eq $username})
+    {
+        Write-Warning "user $fname $lname exists"
+    }
+
+    #create users
+    else
+    {
+    New-ADUser -Name $username -AccountPassword $temppass -ChangePasswordAtLogon $true -Department $dept -DisplayName $fname -EmailAddress $email -GivenName $fname -Surname $lname -Enabled $True
+        #check if department group exists
+        if (Get-ADGroup -F {name -eq $dept})
+        {
+           Write-Warning "group $dept exists"
+        }
+        
+        #create group
+        else
+        {
+           New-ADGroup -GroupScope Universal -Name $dept
+        }
+    #add user to group
+    Add-ADGroupMember -Identity $dept -Members $username
+    }
+
+}
